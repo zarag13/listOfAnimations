f1ad57f9efa86fb5007f594646f79e68a8c4660c
zarag13 <Ksdobnikov@icloud.com>
2023-06-30 19:36:26 +0200

    Initial Commit

---
+++ listOfAnimations/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  listOfAnimations
+//
+//  Created by Kirill on 30.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
